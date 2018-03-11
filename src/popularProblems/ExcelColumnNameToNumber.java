de10682e6366e345b71047a73aacdc3caaeb0e1a
anand <anandinhere@gmail.com>
2018-03-11 15:13:14 -0700

    fixes

--- src/popularProblems/ExcelColumnNameToNumber.java
+++ src/popularProblems/ExcelColumnNameToNumber.java
@@ -5,7 +5,7 @@ public class ExcelColumnNameToNumber {
 
 	public static void main(String[] args) {
 
-		columnNameToNumber("AAZZ");
+		columnNameToNumber("ZZZ");
 	}
 
 	private static void columnNameToNumber(String string) {
--- src/popularProblems/ExcelNumberToColumnName.java
+++ src/popularProblems/ExcelNumberToColumnName.java
@@ -4,23 +4,34 @@ public class ExcelNumberToColumnName {
 	public static int alphabetSize = 26;
 
 	public static void main(String[] args) {
-		int colNbr = 18954;
+		int colNbr = 27;
 
 		excelNumberToColumnName(colNbr);
 	}
 
 	private static void excelNumberToColumnName(int colNbr) {
 
+		if (colNbr < alphabetSize) {
+			System.out.print((char) ('A' + colNbr - 1));
+			return;
+		}
+
 		int newColNbr = (int) (colNbr - getPrevSum(colNbr));
 		int prevPwr = getHigestPower(getPrevSum(colNbr));
 
-		if (newColNbr == 0) { //If perfect sum of squares.
+		if (newColNbr == 0) { // If perfect sum of squares.
 			for (int i = prevPwr; i > 0; i--) {
-				System.out.println((char) (alphabetSize + 'A' - 1));
+				System.out.print((char) (alphabetSize + 'A' - 1));
 			}
 		} else {
 			for (int i = prevPwr; i >= 0; i--) {
-				int highestMultiple = getHighestMultiple(i, newColNbr); //Highest Alphabet * Square less than newColNbr
+				int highestMultiple = getHighestMultiple(i, newColNbr); // Highest
+																		// Alphabet
+																		// *
+																		// Square
+																		// less
+																		// than
+																		// newColNbr
 				newColNbr = (int) (newColNbr - (Math.pow(alphabetSize, i) * highestMultiple));
 				System.out.print((char) ('A' + highestMultiple));
 
@@ -32,11 +43,14 @@ public class ExcelNumberToColumnName {
 	private static int getPrevSum(int colNbr) {
 		int sum = 0;
 		int i = 1;
-		while (sum < colNbr) {
-			sum = sum + (int) Math.pow(alphabetSize, i);
+		while (true) {
+			if (sum + (int) Math.pow(alphabetSize, i) > colNbr)
+				break;
+			else
+				sum = sum + (int) Math.pow(alphabetSize, i);
 			i++;
 		}
-		return sum - (int) Math.pow(alphabetSize, i - 1);
+		return sum;
 	}
 
 	private static int getHighestMultiple(int i, int newColNbr) {
@@ -52,12 +66,11 @@ public class ExcelNumberToColumnName {
 	private static int getHigestPower(int colNbr) {
 		int prevPwr = 0;
 
-		while (Math.pow(alphabetSize, prevPwr) < colNbr) {
+		while (Math.pow(alphabetSize, prevPwr) <= colNbr) {
 			prevPwr = prevPwr + 1;
 		}
 
-		if (prevPwr != 0)
-			prevPwr = prevPwr - 1;
+		prevPwr = prevPwr - 1;
 		return prevPwr;
 	}
 
