0a704476d207c2d98207d0a9bcbffd79082cf668
anand <anandinhere@gmail.com>
2018-05-16 14:46:44 -0700

    Arrays and imports

---
+++ src/array/streams/MedianInStreamOfIntegers.java
@@ -0,0 +1,44 @@
+package array.streams;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+import array.sorting.ArrayUtil;
+
+public class MedianInStreamOfIntegers {
+
+	static PriorityQueue<Integer> pqLeft = new PriorityQueue<Integer>(10, Collections.reverseOrder()); // we want max
+																										// heap on left
+	static PriorityQueue<Integer> pqright = new PriorityQueue<Integer>(10); // by default min heap on right
+
+	public static void main(String[] args) {
+		int[] arr = ArrayUtil.getRandArray(0, 20);
+		System.out.println(Arrays.toString(arr));
+
+		printMedianInStream(arr);
+
+	}
+
+	//Incomplete
+	private static void printMedianInStream(int[] arr) {
+		float median = 0;
+		for (int num : arr) {
+			if (num > median) {
+				pqright.add(num);
+			} else {
+				pqLeft.add(num);
+			}
+
+			if (pqright.size() == pqLeft.size()) {
+				median = (pqright.peek() + pqright.peek()) / 2;
+			} else {
+				PriorityQueue<Integer> biggerQueue = pqright.size() > pqLeft.size() ? pqright : pqLeft;
+				median = biggerQueue.peek();
+			}
+			System.out.println("median " + median);
+
+		}
+
+	}
+}
--- src/util/UsualImports.java
+++ src/util/UsualImports.java
@@ -6,12 +6,25 @@ import java.lang.Math;
 import java.util.Map.Entry;
 import java.util.Arrays;
 import java.util.stream.Stream;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.*;
 
 public class UsualImports {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Random r = new Random();
 
+		ReentrantLock lock = new ReentrantLock();
+		boolean isLockAcquired = lock.tryLock(1, TimeUnit.SECONDS);
+		ReadWriteLock lock1 = new ReentrantReadWriteLock();
+	    //...
+	    Lock writeLock = lock1.writeLock();
+	    
+	    Condition stackEmptyCondition = lock.newCondition();
+	    Condition stackFullCondition = lock.newCondition();
+		
 		HashMap<Integer, String> m = new HashMap<Integer, String>();
 		int[] test = new int[] { 1, 2, 3 };
 		System.out.println(Arrays.toString(test));
@@ -19,6 +32,7 @@ public class UsualImports {
 			System.out.println(e.getKey());
 			Math.max(3, 4);
 		}
+	
 	}
 
 }
