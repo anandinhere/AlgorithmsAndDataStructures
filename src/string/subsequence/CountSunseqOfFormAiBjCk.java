bb80d41f28daf7f5d97c433c11272aa69029347c
anand <anandinhere@gmail.com>
2018-05-29 07:04:56 -0700

    Fix - Rename

--- src/string/subsequence/CountSunseqOfFormAiBjCk.java
+++ src/string/subsequence/CountSunseqOfFormAiBjCk.java
@@ -1,6 +1,6 @@
 package string.subsequence;
 
 //https://discuss.codechef.com/questions/92066/help-needed-for-solving-string-and-sub-sequences-problem?page=1#92116
-public class CountSunseqOfFormaAiBjCk {
+public class CountSunseqOfFormAiBjCk {
 
 }
