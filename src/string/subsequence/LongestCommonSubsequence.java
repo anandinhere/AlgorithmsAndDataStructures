ba649f945e20e5496a914795ca38e6c9ddfd0e0c
anand <anandinhere@gmail.com>
2018-05-29 07:04:04 -0700

    Longest Common Subsequence - Dynamic Programming

--- src/string/subsequence/LongestCommonSubsequence.java
+++ src/string/subsequence/LongestCommonSubsequence.java
@@ -1,16 +1,37 @@
 package string.subsequence;
 
 public class LongestCommonSubsequence {
+
+	static int recurseCount = 0;
+
 	public static void main(String[] args) {
 
 		String str1 = "abcd";
-		String str2 = "abfgc34def";
+		String str2 = "efghijkl";
 		int l = getLengthOfLCS(str1, str2, str1.length(), str2.length());
 		System.out.println(l);
 
-		int[][] lcsDP = new int[str1.length()][str2.length()];
+		int[][] lcsDP = new int[str1.length() + 1][str2.length() + 1];
+		
+		initDPMatrix(str1, str2, lcsDP);
+
 		int lenDP = getLengthOfLCSUsingDP(str1, str2, str1.length(), str2.length(), lcsDP);
 		System.out.println(lenDP);
+
+		System.out.println("recurseCount " + recurseCount);
+		
+	}
+
+	private static void initDPMatrix(String str1, String str2, int[][] lcsDP) {
+		//Initializing DP matrix to -1
+		for (int i = 0; i <= str1.length(); i++) {
+			for (int j = 0; j <= str2.length(); j++) {
+				if (i == 0 || j == 0) {
+					lcsDP[i][j] = 0;
+				}
+				lcsDP[i][j] = -1;
+			}
+		}
 	}
 
 	// No DP
@@ -29,17 +50,23 @@ public class LongestCommonSubsequence {
 
 	private static int getLengthOfLCSUsingDP(String str1, String str2, int m, int n, int[][] lcsDP) {
 
+		recurseCount++;
 		if (m == 0 || n == 0) {
 			return 0;
 		}
+
+		if (lcsDP[m][n] != -1) {
+			return lcsDP[m][n];
+		}
+
 		if (str1.charAt(m - 1) == str2.charAt(n - 1)) {
-			lcsDP[m - 1][n - 1] = getLengthOfLCSUsingDP(str1, str2, m - 1, n - 1, lcsDP) + 1;
+			lcsDP[m][n] = getLengthOfLCSUsingDP(str1, str2, m - 1, n - 1, lcsDP) + 1;
 		} else {
-			lcsDP[m - 1][n - 1] = Math.max(getLengthOfLCSUsingDP(str1, str2, m - 1, n, lcsDP),
+			lcsDP[m][n] = Math.max(getLengthOfLCSUsingDP(str1, str2, m - 1, n, lcsDP),
 					getLengthOfLCSUsingDP(str1, str2, m, n - 1, lcsDP));
 		}
 
-		return lcsDP[m - 1][n - 1];
+		return lcsDP[m][n];
 	}
 
 }
