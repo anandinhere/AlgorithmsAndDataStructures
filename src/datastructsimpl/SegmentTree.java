832cd796dcb26cc82d172f76ebca6aebe2668145
anand <anandinhere@gmail.com>
2018-08-31 21:47:26 -0700

    Segment Tree

--- src/datastructsimpl/SegmentTree.java
+++ src/datastructsimpl/SegmentTree.java
@@ -8,11 +8,34 @@ public class SegmentTree {
 		int[] arr = ArrayUtil.getRandArray(0, 10);
 		ArrayUtil.printArray(arr);
 
-		int segmentTreeSize = getClosestPowerOf2(arr.length);
+		
+		// size of segment tree = 2 * (closest power to 2 to given array)
+		int segmentTreeSize = 2 * getClosestPowerOf2(arr.length);
 		System.out.println(segmentTreeSize);
 
 		int[] segmentTreeArr = new int[segmentTreeSize];
 
+		int totalSum = segmentTree(segmentTreeArr, arr, 0, segmentTreeSize - 1, 0, arr.length - 1);
+		
+		ArrayUtil.printArray(segmentTreeArr);
+		
+		System.out.println(totalSum);
+
+	}
+
+	private static int segmentTree(int[] tree, int[] arr, int treeStart, int treeEnd, int arrStart, int arrEnd) {
+
+		if (arrStart == arrEnd) {
+			tree[treeStart] = arr[arrStart];
+			return tree[treeStart];
+		}
+
+		int mid = (arrStart + arrEnd) / 2;
+
+		tree[treeStart] = segmentTree(tree, arr, 2 * treeStart + 1, treeEnd, arrStart, mid)
+				+ segmentTree(tree, arr, 2 * treeStart + 2, treeEnd, mid + 1, arrEnd);
+
+		return tree[treeStart];
 	}
 
 	private static int getClosestPowerOf2(int length) {
@@ -28,6 +51,6 @@ public class SegmentTree {
 			count++;
 		}
 
-		return (int) Math.pow(2, count );
+		return (int) Math.pow(2, count);
 	}
 }
